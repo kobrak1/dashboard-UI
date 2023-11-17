de19ea88811cf73a7bbc89e32be4a0eff674dc47
kobrak1 <burakkarhan657@gmail.com>
2023-11-17 20:46:59 +0300

    Total charts are done

--- src/components/Total/Total.jsx
+++ src/components/Total/Total.jsx
@@ -7,7 +7,13 @@ const Total = () => {
   return (
     <div className="main-total">
       <div className="total-item">
-        <span className="down">
+        <span
+          className="down"
+          style={{
+            backgroundColor: "rgba(63, 70, 247, 1)",
+            boxShadow: "0 0 15px rgba(4, 177, 235, 0.8)",
+          }}
+        >
           <span className="material-symbols-outlined">south_west</span>
         </span>
         <div className="visits">
@@ -21,46 +27,60 @@ const Total = () => {
         </div>
       </div>
       <div className="total-item-2">
-        <span className="down">
+        <span
+          className="up"
+          style={{
+            backgroundColor: "rgba(254, 135, 67, 1)",
+            boxShadow: "0 0 15px rgba(235, 177, 4, 0.8)",
+          }}
+        >
           <span className="material-symbols-outlined">north_east</span>
         </span>
         <div className="visits">
-          <p style={{ color: "#c2c4c2" }}>Total Visits</p>
+          <p style={{ color: "#c2c4c2" }}>Total Subscribers</p>
           <div className="total-visits" style={{ color: "#e6e8e6" }}>
             {totalVisits}
           </div>
         </div>
-        <div className="rate">
-          <p style={{ margin: "0 auto" }}>+1.29%</p>
+        <div className="rate-2">
+          <p style={{ margin: "0 auto" }}>-0.53%</p>
         </div>
       </div>
       <div className="total-item-3">
-        <span className="down">
+        <span
+          className="active-users"
+          style={{
+            backgroundColor: "rgba(0, 197, 67, 1)",
+            boxShadow: "0 0 15px rgba(54, 208, 54)",
+          }}
+        >
           <span className="material-symbols-outlined">podcasts</span>
         </span>
         <div className="visits">
-          <p style={{ color: "#c2c4c2" }}>Total Visits</p>
+          <p style={{ color: "#c2c4c2" }}>Active Users</p>
           <div className="total-visits" style={{ color: "#e6e8e6" }}>
             {totalVisits}
           </div>
         </div>
-        <div className="rate">
-          <p style={{ margin: "0 auto" }}>+1.29%</p>
-        </div>
+        <div style={{ width: "63px", margin: "0" }}></div>
       </div>
-      <div className="total-item-3">
-        <span className="down">
+      <div className="total-item-4">
+        <span
+          className="total-clicks"
+          style={{
+            backgroundColor: "rgba(137, 35, 217, 1)",
+            boxShadow: "0 0 15px rgba(185, 54, 208)",
+          }}
+        >
           <span className="material-symbols-outlined">monitoring</span>
         </span>
         <div className="visits">
-          <p style={{ color: "#c2c4c2" }}>Total Visits</p>
+          <p style={{ color: "#c2c4c2" }}>Total Clicks</p>
           <div className="total-visits" style={{ color: "#e6e8e6" }}>
             {totalVisits}
           </div>
         </div>
-        <div className="rate">
-          <p style={{ margin: "0 auto" }}>+1.29%</p>
-        </div>
+        <div style={{ width: "63px", margin: "0" }}></div>
       </div>
     </div>
   );
